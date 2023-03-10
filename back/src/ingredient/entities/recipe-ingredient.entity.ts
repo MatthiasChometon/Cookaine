9d92fd050e6103e1a85194dffcc2d15c518041dc
MatthiasChometon <matthiaschometon787@gmail.com>
2023-03-10 17:32:10 +0100

    feat: add ingredient module

---
+++ back/src/ingredient/entities/recipe-ingredient.entity.ts
@@ -0,0 +1,26 @@
+import { Field, Int, ObjectType } from "@nestjs/graphql"
+import { BaseEntity } from "src/database/objects/base-entity"
+import { Recipe } from "src/recipe/entities/recipe.entity"
+import { Column, Entity, ManyToOne } from "typeorm"
+import { MesureUnit } from "../enums/mesure-unit.enum"
+import { Ingredient } from "./ingredient.entity"
+
+@Entity({ name: 'recipeIngredient' })
+@ObjectType()
+export class RecipeIngredient extends BaseEntity {
+    @Field(() => Recipe)
+    @ManyToOne(() => Recipe, ({ recipeIngredients }) => recipeIngredients)
+    recipe: Recipe
+
+    @Field(() => Ingredient)
+    @ManyToOne(() => Ingredient, ({ recipeIngredients }) => recipeIngredients)
+    ingredient: Ingredient
+
+    @Column({ type: 'int' })
+    @Field(() => Int)
+    quantity: number
+
+    @Column({ type: 'enum', enum: MesureUnit })
+    @Field(() => MesureUnit)
+    mesureUnit: MesureUnit
+}
