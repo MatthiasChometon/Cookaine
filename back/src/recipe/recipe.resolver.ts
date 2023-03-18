a0d968a701e2cd2363f88edf5b1cfacefbb90e5a
MatthiasChometon <matthiaschometon787@gmail.com>
2023-03-19 00:34:50 +0100

    feat: add recipes

--- back/src/recipe/recipe.resolver.ts
+++ back/src/recipe/recipe.resolver.ts
@@ -1,13 +1,22 @@
-import { Query, Resolver } from '@nestjs/graphql'
+import { Args, Query, Resolver } from '@nestjs/graphql'
 import { RecipeService } from './recipe.service'
 import { Recipe } from './entities/recipe.entity'
+import { RecipeSearchInput } from './dto/recipe-search.input'
+import { RecipeOutput } from './dto/recipe.output'
+import { IsPublic } from 'src/auth/guards/is-public.guard'
+import { CurrentUser } from 'src/auth/decorators/current-user.decorator'
+import { User } from 'src/user/methods/user.methods'
 
-@Resolver()
+@Resolver(() => Recipe)
 export class RecipeResolver {
 	constructor(private readonly recipeService: RecipeService) {}
 
-	@Query(() => [Recipe])
-	recipes(): Promise<Recipe[]> {
-		return this.recipeService.all()
+	@Query(() => [RecipeOutput])
+	@IsPublic()
+	recipes(
+		@Args('options') options: RecipeSearchInput,
+		@CurrentUser() { id }: User,
+	): Promise<RecipeOutput[]> {
+		return this.recipeService.search(options, id)
 	}
 }
