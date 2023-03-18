5cfe1e2a4edee2242a7a7f33c5219dafccff0f1d
MatthiasChometon <matthiaschometon787@gmail.com>
2023-03-19 00:28:58 +0100

    feat: add tags resolver for admin

--- back/src/tag/tag.resolver.ts
+++ back/src/tag/tag.resolver.ts
@@ -1,7 +1,20 @@
-import { Resolver } from '@nestjs/graphql'
-import { TagService } from './tag.service'
+import { Query, Resolver } from '@nestjs/graphql'
+import { InjectRepository } from '@nestjs/typeorm'
+import { Tag } from './entities/tag.entity'
+import { Repository } from 'typeorm'
+import { UseGuards } from '@nestjs/common'
+import { IsAdminGuard } from 'src/user/guards/is-admin.guard'
 
-@Resolver()
+@Resolver(Tag)
 export class TagResolver {
-	constructor(private readonly tagService: TagService) {}
+	constructor(
+		@InjectRepository(Tag)
+		private tagRepository: Repository<Tag>,
+	) {}
+
+	@Query(() => [Tag])
+	@UseGuards(IsAdminGuard)
+	tags(): Promise<Tag[]> {
+		return this.tagRepository.find()
+	}
 }
