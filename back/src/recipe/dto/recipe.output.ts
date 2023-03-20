ebe7de919a9da5b0dc16eb0bb276ca8317bfcc30
MatthiasChometon <matthiaschometon787@gmail.com>
2023-03-20 01:34:43 +0100

    feat: add tags

--- back/src/recipe/dto/recipe.output.ts
+++ back/src/recipe/dto/recipe.output.ts
@@ -1,9 +1,13 @@
 import { Field, ObjectType, OmitType } from '@nestjs/graphql'
 import { Recipe } from '../entities/recipe.entity'
-import { IngredientOuput } from './ingredient.output'
+import { IngredientOutput } from './ingredient.output'
+import { TagOutput } from 'src/tag/dto/tag.output'
 
 @ObjectType()
-export class RecipeOutput extends OmitType(Recipe, ['recipeIngredients']) {
-	@Field(() => [IngredientOuput], { defaultValue: [] })
-	ingredients: IngredientOuput[]
+export class RecipeOutput extends OmitType(Recipe, ['recipeIngredients', 'recipeTags']) {
+	@Field(() => [IngredientOutput], { defaultValue: [] })
+	ingredients: IngredientOutput[]
+
+	@Field(() => [TagOutput], { defaultValue: [] })
+	tags: TagOutput[]
 }
