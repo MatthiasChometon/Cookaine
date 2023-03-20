a1d13013a0cd00333373d7861f25bc46f18a0291
MatthiasChometon <matthiaschometon787@gmail.com>
2023-03-20 01:36:07 +0100

    feat: update module for recipe

--- back/src/recipe/recipe.module.ts
+++ back/src/recipe/recipe.module.ts
@@ -4,9 +4,12 @@ import { RecipeResolver } from './recipe.resolver'
 import { TypeOrmModule } from '@nestjs/typeorm'
 import { Recipe } from './entities/recipe.entity'
 import { IngredientModule } from 'src/ingredient/ingredient.module'
+import { TagModule } from 'src/tag/tag.module'
+import { Tag } from 'src/tag/entities/tag.entity'
+import { RecipeTag } from 'src/tag/entities/recipe-tag.entity'
 
 @Module({
-	imports: [TypeOrmModule.forFeature([Recipe]), IngredientModule],
+	imports: [TypeOrmModule.forFeature([Recipe, Tag, RecipeTag]), IngredientModule, TagModule],
 	providers: [RecipeResolver, RecipeService],
 })
 export class RecipeModule {}
