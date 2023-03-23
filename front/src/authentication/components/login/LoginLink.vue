ff478f4735d89da1c8c2fddb3d8cc6da29d58ab9
MatthiasChometon <matthiaschometon787@gmail.com>
2023-03-23 18:37:33 +0100

    feat: add components and pages for authentication

---
+++ front/src/authentication/components/login/LoginLink.vue
@@ -0,0 +1,8 @@
+<template>
+	<div class="flex justify-center">
+		Déjà un compte ?
+		<router-link class="text-primary" style="margin-left: 3px" to="/login">
+			Connectez vous ici
+		</router-link>
+	</div>
+</template>
---
+++ front/src/authentication/components/register/RegisterForm.vue
@@ -0,0 +1,51 @@
+<script setup lang="ts">
+import { useMutation } from '@vue/apollo-composable'
+import { REGISTER } from '~/authentication/graphql/register'
+import { useNotification } from '~/common/stores/notification'
+const { sendNotification } = useNotification()
+const router = useRouter()
+
+const newUser = $ref({
+	password: '',
+	email: '',
+	firstName: '',
+	lastName: '',
+})
+
+const { mutate: register, onDone } = useMutation(REGISTER, {
+	fetchPolicy: 'network-only',
+})
+
+onDone((result) => {
+	sendNotification(
+		result,
+		'Un code de validation a été envoyé à votre adresse mail',
+		'Une erreur est survenue lors de la créationde de votre compte',
+	)
+	if (result.errors) return
+
+	router.replace(`/validate-account/${result.data.register.email}`)
+})
+</script>
+
+<template>
+	<div class="full-height flex justify-center items-center column no-wrap">
+		<div class="flex justify-center text-h5 q-pb-md text-primary">Connexion</div>
+		<q-form class="q-gutter-md q-pt-md" @submit="register({ input: newUser })">
+			<EmailInput
+				:email="newUser.email"
+				@update="(email) => (newUser.email = email)"
+			/>
+			<PasswordConfirmationInput
+				:password="newUser.password"
+				@update="(password) => (newUser.password = password)"
+			/>
+
+			<q-card-actions align="center">
+				<q-btn label="Créer son compte" type="submit" color="primary" />
+			</q-card-actions>
+
+			<LoginLink />
+		</q-form>
+	</div>
+</template>
