6ca44313aa9c4758515080005cdc643037e267d0
MatthiasChometon <matthiaschometon787@gmail.com>
2023-03-27 12:45:41 +0200

    feat: update filter for recipes

--- back/src/tag/entities/tag.entity.ts
+++ back/src/tag/entities/tag.entity.ts
@@ -1,12 +1,13 @@
 import { Field, ObjectType } from '@nestjs/graphql'
 import { BaseEntity } from 'src/database/objects/base-entity'
-import { Column, Entity, OneToMany } from 'typeorm'
+import { Column, Entity, Index, OneToMany } from 'typeorm'
 import { RecipeTag } from './recipe-tag.entity'
 
 @Entity({ name: 'tag' })
 @ObjectType()
 export class Tag extends BaseEntity {
 	@Field()
+	@Index()
 	@Column()
 	name: string
 
