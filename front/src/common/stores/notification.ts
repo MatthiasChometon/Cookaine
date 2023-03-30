aa6d7c9a90c21791458d386669a41f6ac5baf91a
MatthiasChometon <matthiaschometon787@gmail.com>
2023-03-30 10:14:29 +0200

    feat: add notification store

--- front/src/common/stores/notification.ts
+++ front/src/common/stores/notification.ts
@@ -1,5 +1,4 @@
-import { acceptHMRUpdate, defineStore } from 'pinia'
-import { useQuasar } from 'quasar'
+import { defineStore } from 'pinia'
 import type { GraphqlResult } from '../types/graphql/GraphqlResult'
 
 export const useNotification = defineStore('notification', () => {
@@ -15,8 +14,9 @@ export const useNotification = defineStore('notification', () => {
 		$q.notify({ color, textColor: 'white', message })
 	}
 
-	return { sendNotification }
-})
+	function sendError(message: string) {
+		$q.notify({ color: 'warning', textColor: 'white', message })
+	}
 
-if (import.meta.hot)
-	import.meta.hot.accept(acceptHMRUpdate(useNotification, import.meta.hot))
+	return { sendNotification, sendError }
+})
