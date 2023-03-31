935246c85fe462365809ae6f2801204f33d9a9b6
MatthiasChometon <matthiaschometon787@gmail.com>
2023-03-31 10:55:10 +0200

    feat: remove auto generated files

--- front/src/components.d.ts
+++ front/src/components.d.ts
@@ -5,13 +5,12 @@ import '@vue/runtime-core'
 
 declare module '@vue/runtime-core' {
   export interface GlobalComponents {
-    CounterPortion: typeof import('./recipe/pages/recipe/components/counterPortion.vue')['default']
-    EmailInput: typeof import('./common/components/forms/EmailInput.vue')['default']
     BurgerMenuButton: typeof import('./layout/components/BurgerMenuButton.vue')['default']
     BurgerMenuLinks: typeof import('./layout/components/BurgerMenuLinks.vue')['default']
     CardRecipe: typeof import('./common/components/forms/CardRecipe.vue')['default']
     CloseDrawer: typeof import('./layout/components/CloseDrawer.vue')['default']
     ConnexionButton: typeof import('./layout/components/ConnexionButton.vue')['default']
+    CounterPortion: typeof import('./recipe/pages/recipe/components/counterPortion.vue')['default']
     DeconnexionButton: typeof import('./layout/components/DeconnexionButton.vue')['default']
     DesktopNavbar: typeof import('./layout/components/DesktopNavbar.vue')['default']
     DifficultiesFilter: typeof import('./recipe-list/components/filters/recipe-list-filter/filters/DifficultiesFilter.vue')['default']
@@ -23,6 +22,7 @@ declare module '@vue/runtime-core' {
     IngredientsFilter: typeof import('./recipe-list/components/filters/recipe-list-filter/filters/IngredientsFilter.vue')['default']
     Input: typeof import('./recipe-new/components/Input.vue')['default']
     Link: typeof import('./layout/components/Link.vue')['default']
+    ListIngredients: typeof import('./recipe/pages/recipe/components/listIngredients.vue')['default']
     LoginForm: typeof import('./authentication/components/login/LoginForm.vue')['default']
     LoginLink: typeof import('./authentication/components/login/LoginLink.vue')['default']
     MaximumTimeCookingFilter: typeof import('./recipe-list/components/filters/recipe-list-filter/filters/MaximumTimeCookingFilter.vue')['default']
@@ -31,7 +31,6 @@ declare module '@vue/runtime-core' {
     Navbar: typeof import('./layout/components/Navbar.vue')['default']
     NavbarIcon: typeof import('./layout/components/NavbarIcon.vue')['default']
     NavbarLinks: typeof import('./layout/components/NavbarLinks.vue')['default']
-    NavbarSearch: typeof import('./layout/components/NavbarSearch.vue')['default']
     PaginationFilter: typeof import('./recipe-list/components/pagination/PaginationFilter.vue')['default']
     PasswordConfirmationInput: typeof import('./common/components/forms/PasswordConfirmationInput.vue')['default']
     RecipeCreatorConnectedUserFilter: typeof import('./recipe-list/components/filters/recipe-list-filter/filters/RecipeCreatorConnectedUserFilter.vue')['default']
@@ -39,6 +38,7 @@ declare module '@vue/runtime-core' {
     RecipeList: typeof import('./recipe-list/components/list/RecipeList.vue')['default']
     RecipeListFilter: typeof import('./recipe-list/components/filters/recipe-list-filter/RecipeListFilter.vue')['default']
     RecipeListWithFilter: typeof import('./recipe-list/components/RecipeListWithFilter.vue')['default']
+    RecipeSearch: typeof import('./recipe-list/components/filters/inputs/RecipeSearch.vue')['default']
     RegisterForm: typeof import('./authentication/components/register/RegisterForm.vue')['default']
     RegisterLink: typeof import('./authentication/components/register/RegisterLink.vue')['default']
     RouterLink: typeof import('vue-router')['RouterLink']
